3dc9c054cb05da703de97cb21ed25d81e1b1a334
sharadnatarajSJSU <sharad.nataraj@sjsu.edu>
2023-10-31 14:07:17 -0700

    HTTP status OK

--- mvp/test_handler.py
+++ mvp/test_handler.py
@@ -33,7 +33,7 @@ mongoengine.connect('cat_exam', host='mongodb://localhost:27017') ## need to cha
 @test_bp.route('/submitresponse', methods=['OPTIONS'])
 @test_bp.route("/get_paper_number", methods = ['OPTIONS'])
 @test_bp.after_request
-def handle_options():
+def handle_options(response):
     origin = request.headers.get('Origin')
     if request.method == 'OPTIONS':
         response = make_response()
