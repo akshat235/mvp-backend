208d4e326f176c2159788fc23a6666b7e7db39b8
sharadnatarajSJSU <sharad.nataraj@sjsu.edu>
2023-10-31 18:13:04 -0700

    comma

--- mvp/test_handler.py
+++ mvp/test_handler.py
@@ -9,7 +9,7 @@ from flask import make_response
 
 
 
-test_bp = Blueprint("test_handler", __name__ )
+test_bp = Blueprint("test_handler", __name__ ,)
 #CORS(test_bp)  
 mongoengine.connect('response', host='mongodb+srv://sequio:nkY9KPEPZmNhBYYx@cluster0.jyovip1.mongodb.net/?retryWrites=true&w=majority/user') 
 
--- mvp/wsgi.py
+++ mvp/wsgi.py
@@ -2,7 +2,6 @@ from app import app
 from flask_cors import CORS, cross_origin 
  
 if __name__ == "__main__":
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-    app.config['CORS_HEADERS'] = 'Content-Type'
+    
     app.run()
         
